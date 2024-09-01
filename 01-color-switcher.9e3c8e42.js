e933eb819cddb4788d8d7160c6bb18d2599ff87c
ghvcbvvb <165384111+ghvcbvvb@users.noreply.github.com>
2024-09-01 05:41:43 +0000

    Deploying to gh-pages from @ ghvcbvvb/goit-js-hw-09@c689e5ead732df51865c4b86d70aed70e9e1f5b0 🚀

---
+++ 01-color-switcher.9e3c8e42.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(e),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.9e3c8e42.js.map
